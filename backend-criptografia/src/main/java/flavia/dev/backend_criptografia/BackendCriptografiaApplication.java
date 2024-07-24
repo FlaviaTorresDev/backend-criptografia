29138ccac8b6c40dfe6abfcc918f8d5be0bd3748
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-23 23:08:02 -0300

    first commit

---
+++ backend-criptografia/src/main/java/flavia/dev/backend_criptografia/BackendCriptografiaApplication.java
@@ -0,0 +1,13 @@
+package flavia.dev.backend_criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCriptografiaApplication.class, args);
+	}
+
+}
---
+++ backend-criptografia/src/test/java/flavia/dev/backend_criptografia/BackendCriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package flavia.dev.backend_criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
